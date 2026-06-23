edb75410d400a8d900ea2793647f8c8f7a4ac017
raqsoli <rqoliveira12@gmail.com>
2026-06-23 10:09:04 -0300

    Adiciona visualização e edição de perfil

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
-from .views import RegisterView
+
+from .views import (
+    RegisterView,
+    ProfileView
+)
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
+    path(
+        "register/",
+        RegisterView.as_view(),
+        name="register"
+    ),
+
+    path(
+        "profile/",
+        ProfileView.as_view(),
+        name="profile"
+    ),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,24 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-from .serializers import RegisterSerializer
+from .serializers import (
+    RegisterSerializer,
+    ProfileSerializer
+)
 
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+
+    permission_classes = [
+        IsAuthenticated
+    ]
+
+    def get_object(self):
+        return self.request.user
